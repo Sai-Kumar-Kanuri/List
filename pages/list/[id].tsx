090d2d9b0c2af671974be2326454b7d967f5daea
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-05-19 23:21:45 +0530

    final

--- pages/list/[id].tsx
+++ pages/list/[id].tsx
@@ -1,41 +1,39 @@
 export const getStaticPaths = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-  
-    // map data to an array of path objects with params (id)
-    const paths = data.map(list => {
-      return {
-        params: { id: list.id.toString() }
-      }
-    })
-  
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map(ninja => {
     return {
-      paths,
-      fallback: false
+      params: { id: ninja.id.toString() }
     }
-  }
+  })
 
-  export const getStaticProps = async(context) =>{
-     const id = context.params.id;
-     const res = fetch('https://jsonplaceholder.typicode.com/users/'+id);
-     const data = await (await res).json();
-
-     return {
-        props:{list :data}
-     }
-
+  return {
+    paths,
+    fallback: false
   }
-  
-  
-  const Details = ({list}) => {
-    return (
-      <div>
-        <h1>{list.name}</h1>
-        <p>{list.email}</p>
-        <p>{list.website}</p>
-        <p>{list.address.city}</p>
-      </div>
-    );
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
   }
-  
-  export default Details;
+}
+
+const Details = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p>
+    </div>
+  );
+}
+
+export default Details;
